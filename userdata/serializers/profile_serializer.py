1cef9024e3fbaa5ca1281d8aa4201bde97c301c1
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-26 08:46:27 -0300

    feat: adicionando serializer method para exibir o username ao invés do ID no corpo da resposta de following e follower em /api/users/username/

--- userdata/serializers/profile_serializer.py
+++ userdata/serializers/profile_serializer.py
@@ -3,6 +3,15 @@ from ..models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    following = serializers.SerializerMethodField()
+    followers = serializers.SerializerMethodField()
+
     class Meta:
         model = Profile
         fields = ['avatar', 'bio', 'birthdate', 'cover', 'name', 'following', 'followers']
+
+    def get_following(self, obj):
+        return [profile.user.username for profile in obj.following.all()]
+
+    def get_followers(self, obj):
+        return [profile.user.username for profile in obj.followers.all()]
