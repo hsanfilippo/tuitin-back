3f0c72e547a2ada6afa9f89403045b0e550558c8
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-26 16:47:52 -0300

    feat: add ProfileSerializer como nested de UserSerializer

--- userdata/serializers/user_serializer.py
+++ userdata/serializers/user_serializer.py
@@ -1,9 +1,27 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from django.contrib.auth.models import User
+from .profile_serializer import ProfileSerializer
 
 class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer()
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
-        read_only_fields = ['id']
+        fields = ['id', 'username', 'email', 'profile']
+        read_only_fields = ['id', 'username', 'email']
+
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile', {})
+
+        # Atualiza o próprio usuário
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        # Atualiza o perfil relacionado
+        profile = instance.profile
+        for attr, value in profile_data.items():
+            setattr(profile, attr, value)
+        profile.save()
+
+        return instance
