a1edb941ae43601bbd2f0b957c09a81037c915c4
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-28 10:40:14 -0300

    feat: add likes ao model de posts

--- posts/models/post.py
+++ posts/models/post.py
@@ -10,6 +10,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     content = models.CharField(max_length=255, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
 
     def __str__(self):
         return f'{self.author.username}: {self.content}'
