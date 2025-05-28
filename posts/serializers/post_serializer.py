5f2e0990d538bceef5aa75edd36f17080eb65cb9
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-28 10:42:12 -0300

    chore: implementando view e serializer de like e comentarios em posts

--- posts/serializers/post_serializer.py
+++ posts/serializers/post_serializer.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from posts.models.post import Post
+from posts.serializers.comentarios_serializer import ComentariosSerializer
+
 
 class PostSerializer(serializers.ModelSerializer):
     author_username = serializers.CharField(source='author.username', read_only=True)
+    author_avatar = serializers.CharField(source='author.profile.avatar', read_only=True)
+    author_name = serializers.CharField(source='author.profile.name', read_only=True)
+    comentarios = ComentariosSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
         fields = '__all__'
-        read_only_fields = ['author', 'created_at']
+        read_only_fields = ['author', 'created_at', 'like_count', 'comentarios']
--- posts/viewsets/post_viewset.py
+++ posts/viewsets/post_viewset.py
@@ -1,4 +1,8 @@
 from rest_framework import generics, permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from ..models import Post
 from ..serializers import PostSerializer
@@ -12,10 +16,29 @@ class ListarCriarPosts(generics.ListCreateAPIView):
         serializer.save(author=self.request.user)
 
 
-class EditarExcluirPosts(generics.RetrieveDestroyAPIView):
+class EditarExcluirPosts(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     lookup_field = 'id'
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         return Post.objects.all().order_by("created_at")
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def toggle_like(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    user = request.user
+
+    if user in post.likes.all():
+        post.likes.remove(user)
+        liked = False
+    else:
+        post.likes.add(user)
+        liked = True
+
+    return Response({
+        'liked': liked,
+        'like_count': post.likes.count(),
+    })
